f92cf32740974fbfbb79fe20439422c53c14f6b4
River Kopp <caleb.m.kopp@outlook.com>
2026-08-15 02:30:22 +0000

    make sync (#59)
    * make sync
    
    * make sync
    
    * make sync
    
    * make sync
    
    * make sync
    
    ---------
    
    Co-authored-by: River Kopp <river.kopp@oracle.com>

--- hack/fresh_install.sh
+++ hack/fresh_install.sh
@@ -4,7 +4,7 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install Brewfile contents
-brew bundle --file="./lists/Brewfile"
+brew bundle --file="./lists/Brewfile" -v
 
 # this should install visual-studio-code, so we can run the vsc_install_list
 /bin/bash ./lists/vsc_install_list.sh
